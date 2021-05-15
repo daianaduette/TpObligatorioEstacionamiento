a852523511a2006dc476963836dd75bec349f9c3
daianaduette <daiana.duette@hotmail.com>
2021-05-14 21:47:28 -0300

    index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 
     
 
-    <!-- Bootstrap core CSS -->
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
 
 
 
